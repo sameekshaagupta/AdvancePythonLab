24c398bbddbe86b15a7c4f814942cbeb7b7b5ec5
Sameeksha Gupta <136142214+Sameekksshhaa@users.noreply.github.com>
2024-08-28 11:15:22 +0530

    average score file

--- 5/average.py
+++ 5/average.py
@@ -9,7 +9,8 @@ with open('5\student_average.csv', mode='r') as file:
         name = row['Name']
         scores = [int(row['Maths']), int(row['Science']), int(row['Hindi'])]
         average_score = calculate_average(scores)
-        students.append({'Name': name, 'Average': average_score})
+        student_dict = {'Name': name, 'Average': average_score}
+        students.append(student_dict)
 
 
 with open('student_average_grade.csv', mode='w', newline='') as file:
